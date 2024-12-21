67006fd69e77b51d00cc063e411d0089c2d383ea
Adil Ansari <adil.ansari@igtsolutions.com>
2024-12-21 11:51:41 +0000

    portfolio v1 initialized

---
+++ src/constants.js
@@ -0,0 +1,74 @@
+export const techStack = [
+
+  
+  { name: "AWS", link: "https://img.icons8.com/?size=100&id=33039&format=png&color=000000" },
+  { name: "Azure", link: "https://img.icons8.com/?size=100&id=VLKafOkk3sBX&format=png&color=000000" },
+  { name: "Python", link: "https://img.icons8.com/?size=100&id=13441&format=png&color=000000" },
+  { name: "Linux", link: "https://img.icons8.com/?size=100&id=WbRVMGxHh74X&format=png&color=000000" },
+  { name: "Postman", link: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" },
+
+  { name: "Jenkins", link: "https://img.icons8.com/?size=100&id=39292&format=png&color=000000" },
+  { name: "Azure DevOps", link: "https://img.icons8.com/?size=100&id=81727&format=png&color=000000" },
+  { name: "Team City", link: "https://www.incredibuild.com/wp-content/uploads/2021/03/TeamCity01-1-150x150.png" },
+  { name: "Octopus", link: "https://a.storyblok.com/f/92909/x/084296103a/logo-blue_140px_rgb.svg" },
+  { name: "GitHub Actions", link: "https://github.gallerycdn.vsassets.io/extensions/github/vscode-github-actions/0.27.0/1727202375538/Microsoft.VisualStudio.Services.Icons.Default" },
+  { name: "Terraform", link: "https://img.icons8.com/?size=100&id=kEkT1u7zTDk5&format=png&color=000000" },
+  { name: "AWS Cloud Formation", link: "https://iconduck.com/api/v2/vectors/vctrhqcl6d1x/media/png/512/download" },
+  { name: "Ansible", link: "https://img.icons8.com/?size=100&id=iGCCE2iEmh2u&format=png&color=000000" },
+
+  { name: "Docker", link: "https://img.icons8.com/?size=100&id=cdYUlRaag9G9&format=png&color=000000" },
+  { name: "Kubernetes", link: "https://img.icons8.com/?size=100&id=cvzmaEA4kC0o&format=png&color=000000" },
+  { name: "RedHat Linux", link: "https://img.icons8.com/?size=100&id=h2fWCnoQXWPf&format=png&color=B60A0A" },
+  { name: "SQL", link: "https://www.flaticon.com/download/icon/4299956?icon_id=4299956&author=1255&team=1255&keyword=Sql+server&pack=4299722&style=1&style_id=1219&format=png&color=%23000000&colored=1&size=512&selection=1&type=standard" },
+  { name: "Mongo DB", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain.svg" },
+  { name: "GitHub", link: "https://img.icons8.com/?size=40&id=62856&format=png&color=000000" },
+  // { name: "Azure", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-plain.svg" },
+  { name: "Git", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg" },
+  // { name: "Docker", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain.svg" },
+
+  { name: "Bash", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg" },
+  { name: "Nginx", link: "https://img.icons8.com/?size=100&id=t2x6DtCn5Zzx&format=png&color=000000" },
+  { name: "Prometheus", link: "https://img.icons8.com/?size=100&id=NrcKwdmlOBfE&format=png&color=000000" },
+  { name: "Grafana", link: "https://img.icons8.com/?size=100&id=9uVrNMu3Zx1K&format=png&color=000000" },
+  { name: "DataDog", link: "https://static-00.iconduck.com/assets.00/datadog-icon-957x1024-ksuqw1rf.png" },
+  // { name: "Vim", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vim/vim-original.svg" },
+
+  // { name: "VS Code", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" },
+  // {name: "", link: "https://repository-images.githubusercontent.com/59065830/b62be480-45d2-11ea-9989-803db0f9c44d"}
+]
+
+
+export const contactLinks = [{ name: 'Dev.To', url: 'https://img.icons8.com/?size=40&id=n98knU41v5Aq&format=png&color=000000', link: 'https://dev.to/adilansari' },
+{ name: 'Github', url: 'https://img.icons8.com/?size=40&id=62856&format=png&color=000000', link :'https://github.com/adilansari488'},
+{ name: 'LinkedIn', url: 'https://img.icons8.com/?size=40&id=13930&format=png&color=000000', link: 'https://www.linkedin.com/in/adilansari488/'}
+]
+
+
+
+export const serviceData = [
+  {
+    name: 'Web Application Development',
+    desc: 'Web design encompasses many different skills and disciplines in the production and maintenance of websites. The different areas of web design include web graphic design user interface design  authoring, including standardised code and proprietary software user experience design and search engine',
+    img: "https://img.icons8.com/external-kiranshastry-lineal-color-kiranshastry/64/000000/external-web-development-coding-kiranshastry-lineal-color-kiranshastry.png"
+  },
+  {
+    name: 'Mobile Application Development',
+    desc: `We provide a range of mobile application development services
+    including custom mobile development on Android platforms, building
+    cross-platform apps, designing user experience and integrating
+    novel mobile interfaces such as chat and voice`,
+    img: "https://img.icons8.com/external-justicon-lineal-color-justicon/64/000000/external-app-development-responsive-web-design-justicon-lineal-color-justicon.png"
+  },
+
+  {
+    name: 'Backend Development',
+    desc: `Graphic design is a craft where professionals create visual
+    content to communicate messages. By applying visual hierarchy and
+    page layout techniques, designers use typography and pictures to
+    meet users' specific needs and focus on the logic of displaying
+    elements in interactive designs, to optimize the user experience.`,
+    img: "https://img.icons8.com/external-flaticons-flat-flat-icons/64/000000/external-backend-no-code-flaticons-flat-flat-icons.png"
+  },
+
+
+]
